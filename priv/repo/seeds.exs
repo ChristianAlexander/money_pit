ae53be61868aa2e9b6b8c30f5f4ef7872e9af721
Christian Alexander <christian@linux.com>
2025-07-16 07:17:02 -0700

    add seed script for default users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+MoneyPit.Commerce.create_product!(%{name: "Pro Plan", price: "19.99"}, authorize?: false)
+MoneyPit.Commerce.create_product!(%{name: "Max Plan", price: "200.00"}, authorize?: false)
+
+{:ok, hashed_password} = AshAuthentication.BcryptProvider.hash("password")
+
+Ash.Seed.seed!(MoneyPit.Accounts.User, %{
+  email: "admin@example.com",
+  hashed_password: hashed_password,
+  role: :admin
+})
+
+Ash.Seed.seed!(MoneyPit.Accounts.User, %{
+  email: "user@example.com",
+  hashed_password: hashed_password
+})
